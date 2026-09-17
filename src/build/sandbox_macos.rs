88eb774041e8a7b2cd1618e086ad2a68990aa08c
Heyang Zhou <hello@su3.io>
2026-09-17 03:41:08 +0000

    Allow macOS loader root lookup and guarded vnode initialization

--- src/build/sandbox_macos.rs
+++ src/build/sandbox_macos.rs
@@ -48,6 +48,10 @@ fn profile(
 (allow process-info* (target self))
 (allow sysctl-read)
 (allow file-read-metadata)
+(allow file-read* (literal "/"))
+(allow system-mac-syscall (mac-policy-name "vnguard"))
+(allow system-mac-syscall
+    (require-all (mac-policy-name "Sandbox") (mac-syscall-number 67)))
 (allow file-map-executable (subpath "/System/Library") (subpath "/usr/lib"))
 (allow file-read* file-map-executable
     (subpath "/System/Volumes/Preboot/Cryptexes/OS/System/Library/dyld")
